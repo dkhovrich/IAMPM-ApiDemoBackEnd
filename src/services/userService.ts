fb1855dbaedc19936e3a4ab53409ab34e9d265c8
Dmitriy Khovrich <dkhovrich@lohika.com>
2018-07-27 17:45:25 +0300

    fix

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,7 +3,7 @@ import { IUserDto, UserDto, User, IUserModel } from '../models/userModel';
 
 class UserService extends BaseService {
     async login(email: string): Promise<IUserDto> {
-        return await this.handleConnection(async () => {
+        return await this.handleConnection<IUserDto>(async () => {
             let user: IUserModel = await User.findOne({ email });
             if (!user) {
                 user = await User.create({ email });
